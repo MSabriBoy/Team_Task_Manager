fea7f78a8a4469a2c2928109088b6c92749f9625
Rihan Sabri <mrihansabri@gmail.com>
2026-05-14 16:50:01 +0530

    fix(auth): manage token state for protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,22 +5,33 @@ import {
   Navigate
 } from "react-router-dom";
 
+import { useEffect, useState } from "react";
+
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import Projects from "./pages/Projects";
 import Tasks from "./pages/Tasks";
 import AppNavbar from "./components/Navbar";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
 
-  const token = localStorage.getItem("token");
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+
+    setToken(
+      localStorage.getItem("token")
+    );
+
+  }, []);
 
   return (
     <BrowserRouter>
-   {token && <AppNavbar />}
+
+      {token && <AppNavbar />}
 
       <Routes>
 
@@ -30,31 +41,31 @@ function App() {
         />
 
         <Route
-  path="/dashboard"
-  element={
-    token
-      ? <Dashboard />
-      : <Navigate to="/" />
-  }
-/>
+          path="/dashboard"
+          element={
+            token
+              ? <Dashboard />
+              : <Navigate to="/" />
+          }
+        />
 
         <Route
-  path="/projects"
-  element={
-    token
-      ? <Projects />
-      : <Navigate to="/" />
-  }
-/>
+          path="/projects"
+          element={
+            token
+              ? <Projects />
+              : <Navigate to="/" />
+          }
+        />
 
         <Route
-  path="/tasks"
-  element={
-    token
-      ? <Tasks />
-      : <Navigate to="/" />
-  }
-/>
+          path="/tasks"
+          element={
+            token
+              ? <Tasks />
+              : <Navigate to="/" />
+          }
+        />
 
       </Routes>
 
