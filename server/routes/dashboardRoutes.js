c4a80b10c21bb6f9cfa35f2db02855abbe907c7c
Rihan Sabri <mrihansabri@gmail.com>
2026-05-14 15:23:01 +0530

    creaded the dashboard route file

---
+++ server/routes/dashboardRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+    getDashboardStats
+} = require("../controllers/dashboardController");
+
+const protect = require("../middleware/authMiddleware");
+
+router.get(
+    "/stats",
+    protect,
+    getDashboardStats
+);
+
+module.exports = router;
