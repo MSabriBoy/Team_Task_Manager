a0b12d42c1c3c15a36562d91f02d84a064d5c346
Rihan Sabri <mrihansabri@gmail.com>
2026-05-14 17:20:54 +0530

    feat(route): add protected users endpoint

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const protect = require("../middleware/authMiddleware");
 
-const { registerUser,loginUser } = require("../controllers/authController");
+const { registerUser,loginUser, getUsers } = require("../controllers/authController");
 
 router.post("/register", registerUser);
-router.post("/login", loginUser)
+router.post("/login", loginUser);
+router.get(
+    "/users",
+    protect,
+    getUsers
+);
 
 module.exports = router;
