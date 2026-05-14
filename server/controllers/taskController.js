6bb764b011b769a1ab15017314873359be17a361
Rihan Sabri <mrihansabri@gmail.com>
2026-05-14 15:12:11 +0530

    added the updated task status fucntion in controllers

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -33,6 +33,38 @@ const createTask = async (req, res) => {
     }
 };
 
+const updateTaskStatus = async (req, res) => {
+    try {
+
+        const { status } = req.body;
+
+        const task = await Task.findByIdAndUpdate(
+            req.params.id,
+            { status },
+            { new: true }
+        );
+
+        if (!task) {
+            return res.status(404).json({
+                message: "Task not found"
+            });
+        }
+
+        res.status(200).json({
+            message: "Task status updated",
+            task
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            message: error.message
+        });
+
+    }
+};
+
 module.exports = {
-    createTask
+    createTask,
+    updateTaskStatus
 };
