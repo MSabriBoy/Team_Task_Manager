6e8ef844f22c9448daf8b738c10cee56e0c4e2c0
Rihan Sabri <mrihansabri@gmail.com>
2026-05-14 15:23:50 +0530

    connect the dashboardRoutes file in server file

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const connectDB= require('./config/db')
 const authRoutes = require('./routes/authRoutes')
 const projectRoutes = require('./routes/projectRoutes')
 const taskRoutes = require("./routes/taskRoutes");
+const dashboardRoutes = require("./routes/dashboardRoutes");
 
 dotenv.config()
 
@@ -17,6 +18,7 @@ app.use(express.json())
 app.use("/api/auth/",authRoutes)
 app.use("/api/projects", projectRoutes)
 app.use("/api/tasks", taskRoutes);
+app.use("/api/dashboard", dashboardRoutes);
 
 app.get("/", (req,res)=>{
     res.json("server working")
