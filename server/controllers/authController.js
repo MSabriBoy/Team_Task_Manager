1d5fed87d2c320094d7cfc9642217cff6b365ffb
Rihan Sabri <mrihansabri@gmail.com>
2026-05-14 17:19:34 +0530

    feat(api): add get users controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -88,7 +88,27 @@ const loginUser = async (req,res)=>{
     
 }
 
+const getUsers = async (req, res) => {
+    try {
+
+        const users = await User.find(
+            {},
+            "-password"
+        );
+
+        res.status(200).json(users);
+
+    } catch (error) {
+
+        res.status(500).json({
+            message: error.message
+        });
+
+    }
+};
+
 module.exports = {
     registerUser,
-    loginUser
+    loginUser,
+    getUsers
 }
