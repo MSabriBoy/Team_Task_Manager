3883646fed9a8e803808e4a9524c135fd3582471
Rihan Sabri <mrihansabri@gmail.com>
2026-05-14 16:37:16 +0530

    feat(auth): add logout functionality in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,41 +1,57 @@
 import {
-  Navbar,
-  Nav,
-  Container
+    Navbar,
+    Nav,
+    Container
 } from "react-bootstrap";
 
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const AppNavbar = () => {
-  return (
-    <Navbar bg="dark" variant="dark">
 
-      <Container>
+    const navigate = useNavigate();
 
-        <Navbar.Brand>
-          Team Task Manager
-        </Navbar.Brand>
+    const handleLogout = () => {
 
-        <Nav>
+        localStorage.removeItem("token");
 
-          <Nav.Link as={Link} to="/dashboard">
-            Dashboard
-          </Nav.Link>
+        navigate("/");
 
-          <Nav.Link as={Link} to="/projects">
-            Projects
-          </Nav.Link>
+    };
 
-          <Nav.Link as={Link} to="/tasks">
-            Tasks
-          </Nav.Link>
+    return (
+        <Navbar bg="dark" variant="dark">
 
-        </Nav>
+            <Container>
 
-      </Container>
+                <Navbar.Brand>
+                    Team Task Manager
+                </Navbar.Brand>
 
-    </Navbar>
-  );
+                <Nav>
+
+                    <Nav.Link as={Link} to="/dashboard">
+                        Dashboard
+                    </Nav.Link>
+
+                    <Nav.Link as={Link} to="/projects">
+                        Projects
+                    </Nav.Link>
+
+                    <Nav.Link as={Link} to="/tasks">
+                        Tasks
+                    </Nav.Link>
+
+                    <Nav.Link onClick={handleLogout}>
+                        Logout
+                    </Nav.Link>
+
+                </Nav>
+
+            </Container>
+
+        </Navbar>
+    );
 };
 
 export default AppNavbar;
